a72d269ee188d7b62edc26ade1464d039111a1aa
wad <eric@wadhome.org>
2022-06-25 21:50:38 -0400

    Initial stuff

---
+++ src/main/java/org/wadhome/digraph/Application.java
@@ -0,0 +1,8 @@
+package org.wadhome.digraph;
+
+public class Application {
+
+    public static void main(String... args) {
+        System.out.println("main reached");
+    }
+}
---
+++ src/test/java/org/wadhome/digraph/ApplicationTest.java
@@ -0,0 +1,12 @@
+package org.wadhome.digraph;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testIt() {
+        Assertions.assertTrue(true);
+    }
+}
